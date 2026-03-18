96f63dd953cc6132e9a271500cab1ac0e21e96f1
HefestusWoodcock <haltonhalton8@gmail.com>
2026-03-18 11:07:09 +0100

    test commit

--- lua/homigrad/playerclass/classes/sh_insurgent.lua
+++ lua/homigrad/playerclass/classes/sh_insurgent.lua
@@ -59,3 +59,75 @@ local subclasses = {
     medic = {}
 
 }
+
+function CLASS.On(self, data)
+    if CLIENT then return end
+
+    ApplyAppearance(self,nil,nil,nil,true)
+    local appearance = self.CurAppearance or hg.Appearance.GetRandomAppearance()
+    appearance.AAttachments = ""
+    appearance.AColthes = ""
+
+    local mdl_key = appearance.AModel
+    if not rebel_models[mdl_key] then
+        self:ChatPrint("zcity/appearance.json have invalid variables.. Setting random Appearance")
+        appearance = hg.Appearance.GetRandomAppearance()
+        mdl_key = appearance.AModel
+    end
+
+    self:SetPlayerColor(Color(13,101,5):ToVector())
+    self:SetModel(rebel_models[mdl_key])
+    self:SetSubMaterial()
+    self:SetNetVar("Accessories", "")
+
+    if not data.bNoEquipment then
+        self:PlayerClassEvent("GiveEquipment", self.subClass)
+    end
+
+
+    if self.subClass == "medic" then
+        local new_mdl = rebel_medic_models[self:GetModel()]
+        if new_mdl then
+            self:SetModel(new_mdl)
+        end
+    end
+
+
+    self.subClass = nil
+
+    if zb and zb.GiveRole then
+        zb.GiveRole(self, "Rebel", Color(0, 173, 43))
+    end
+
+    self:SetBodygroup(10, 1)                  
+    self:SetBodygroup(8, math.random(0,15))   
+    self:SetBodygroup(9, math.random(0,9))    
+    self:SetSkin(math.random(0,3))            
+
+
+    self.CurAppearance = appearance
+    
+    for k,v in ipairs(ents.FindByClass("npc_*")) do
+        if table.HasValue(rebels,v:GetClass()) then
+            v:AddEntityRelationship( self, D_LI, 0 )
+            v:ClearEnemyMemory()
+        elseif table.HasValue(combines,v:GetClass()) then
+            v:AddEntityRelationship( self, D_HT, 99 )
+            v:ClearEnemyMemory()
+        end
+    end
+    
+    local index = self:EntIndex()
+    hook.Add( "OnEntityCreated", "rebel_relation_ship"..index, function( ent )
+        if not IsValid(self) then hook.Remove("OnEntityCreated","rebel_relation_ship"..index) return end
+        if ( ent:IsNPC() ) then
+            if table.HasValue(rebels,ent:GetClass()) then
+                ent:AddEntityRelationship( self, D_LI, 0 )
+            end
+
+            if table.HasValue(combines,ent:GetClass()) then
+                ent:AddEntityRelationship( self, D_HT, 99 )
+            end
+        end
+    end )
+end
